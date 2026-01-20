139b9a236fb35828d27d70907a571bd70790680f
David Read <david2.read@education.gov.uk>
2026-01-20 14:13:50 +0000

    PV2-3823 add tables for GSL short course earnings

---
+++ src/SFA.DAS.Payments.Database/Payments2/Tables/Payments2.ShortCourseEarning.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [Payments2].[ShortCourseEarning]
+(
+	[Id] BIGINT NOT NULL IDENTITY(1,1) CONSTRAINT PK_ShortCourseEarning PRIMARY KEY CLUSTERED,
+	[EarningsId] UNIQUEIDENTIFIER NOT NULL,
+	[UKPRN] BIGINT NOT NULL,
+	[LearnerId] BIGINT NOT NULL,
+	[LearnerUln] BIGINT NOT NULL,
+	[LearnerReference] NVARCHAR(50) NOT NULL,
+	[TrainingType] TINYINT NOT NULL,
+	[CourseCode] INT NOT NULL,
+	[StartDate] DATETIME2 NOT NULL,
+	[AgeAtStartOfTraining] TINYINT NOT NULL,
+	[PlannedEndDate] DATETIME2 NOT NULL,
+	[ActualEndDate] DATETIME2 NULL,
+	[TrainingStatus] TINYINT NOT NULL,
+	[EmployerContribution] DECIMAL(15,5) NOT NULL
+)
