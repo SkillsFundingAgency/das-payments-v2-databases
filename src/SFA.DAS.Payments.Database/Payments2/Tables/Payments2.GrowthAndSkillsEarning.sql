2ae104d6fcbf4ddefd8642ce2e60c7ae9f98ed59
David Read <david2.read@education.gov.uk>
2026-02-19 12:00:51 +0000

    Revert "PV2-3902 add CourseReference to GrowthAndSkillsEarning table"
    This reverts commit 20503649b683347f63ab35630b94cf5e363cccba.

--- src/SFA.DAS.Payments.Database/Payments2/Tables/Payments2.GrowthAndSkillsEarning.sql
+++ src/SFA.DAS.Payments.Database/Payments2/Tables/Payments2.GrowthAndSkillsEarning.sql
@@ -8,7 +8,6 @@
 	[LearnerReference] NVARCHAR(50) NOT NULL,
 	[LearningType] TINYINT NOT NULL,
 	[CourseCode] NVARCHAR(255) NOT NULL,
-	[CourseReference] NVARCHAR(255) NOT NULL,
 	[StartDate] DATETIME2 NOT NULL,
 	[AgeAtStartOfTraining] TINYINT NOT NULL,
 	[PlannedEndDate] DATETIME2 NOT NULL,
