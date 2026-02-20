db9c164930898489d6a5fbf24bbb5cf3fad975c1
syed-wali <6061008+syed-wali@users.noreply.github.com>
2026-02-20 09:27:09 +0000

    PV2-3905 Update CollectionPeriod add Start and End Date

--- src/SFA.DAS.Payments.Database/Payments2/Tables/Payments2.CollectionPeriod.sql
+++ src/SFA.DAS.Payments.Database/Payments2/Tables/Payments2.CollectionPeriod.sql
@@ -8,5 +8,7 @@
 	[AuditMigrationStartDate] DATETIME2 NULL,
 	[AuditMigrationCompletionDate] DATETIME2 NULL,
 	[CreationDate] DATETIME2 NOT NULL DEFAULT sysutcdatetime(), 
-    [Status] TINYINT NULL
+    [Status] TINYINT NULL, 
+    [StartDate] DATETIME2 NULL, 
+    [EndDate] DATETIME2 NULL
 )
