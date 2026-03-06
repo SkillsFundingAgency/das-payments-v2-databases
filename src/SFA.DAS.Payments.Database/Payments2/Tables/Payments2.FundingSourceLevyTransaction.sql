9890cc727c562f80d904faaff6a637366a78fd3d
Mazhar7262 <58623068+Mazhar7262@users.noreply.github.com>
2026-03-06 09:56:00 +0000

    updated length

--- src/SFA.DAS.Payments.Database/Payments2/Tables/Payments2.FundingSourceLevyTransaction.sql
+++ src/SFA.DAS.Payments.Database/Payments2/Tables/Payments2.FundingSourceLevyTransaction.sql
@@ -34,7 +34,7 @@
 	FundingPlatformType TINYINT NULL constraint DF_FundingSourceLevyTransaction__FundingPlatformType DEFAULT(1), 
     [LearningType] TINYINT NULL, 
     [CourseType] TINYINT NULL,
-	[CourseCode] NVARCHAR(8) NULL
+	[CourseCode] NVARCHAR(50) NULL
 )
 GO
 
